1d48643fb66a6b85335933faf6d74446202cc590
name8965 <name8965@naver.com>
2022-02-05 21:35:49 +0900

    6.2 styles

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -39,7 +39,7 @@ const AuthForm = () => {
   const toggleAccount = () => setNewAccount((prev) => !prev);
   return (
     <>
-      <form onSubmit={onSubmit}>
+      <form onSubmit={onSubmit} className="container">
         <input
           name="email"
           type="email"
@@ -47,6 +47,7 @@ const AuthForm = () => {
           required
           value={email}
           onChange={onChange}
+          className="authInput"
         />
         <input
           name="password"
@@ -54,15 +55,17 @@ const AuthForm = () => {
           placeholder="password"
           required
           value={password}
+          className="authInput"
           onChange={onChange}
         />
-        {error}
         <input
           type="submit"
+          className="authInput authSubmit"
           value={newAccount ? "Create Account" : "Sign In"}
         />
+        {error && <span className="authError">{error}</span>}
       </form>
-      <span onClick={toggleAccount}>
+      <span onClick={toggleAccount} className="authSwitch">
         {newAccount ? "Sign In" : "Create Account"}
       </span>
     </>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import { authService } from "fbase";
+import "./styles.css";
 // console.log(firebase);
 
 ReactDOM.render(
